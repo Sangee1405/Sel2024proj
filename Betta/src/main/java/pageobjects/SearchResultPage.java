089bdf74ee04b3c57b767d30f57d5415ca744749
Sangee1405 <svsangee14@gmail.com>
2024-04-22 22:27:37 +1000

    pageobjectcreation

---
+++ Betta/src/main/java/pageobjects/SearchResultPage.java
@@ -0,0 +1,20 @@
+package pageobjects;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class SearchResultPage {
+
+    WebDriver driver;
+    public String getTiltleofSearchPage(){
+
+       return driver.getTitle();
+
+    }
+    public void selectProductFromSearchResultPage(int searchIndex){
+
+        driver.findElement(By.xpath("//div[@data-cel-widget='search_result_"+searchIndex+" ']")).click();
+
+
+    }
+}

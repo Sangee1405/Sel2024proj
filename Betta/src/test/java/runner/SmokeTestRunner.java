56fe107ed13e3aeed1613bdf5d5848805f2fdcaa
Sangee1405 <svsangee14@gmail.com>
2024-04-23 21:57:41 +1000

    maven configuration

---
+++ Betta/src/test/java/runner/SmokeTestRunner.java
@@ -0,0 +1,15 @@
+package runner;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = {"src/test/resources/testdesign"},
+        glue = {"stepdef","hookspack"},tags ="@smoketest",plugin = {"pretty"}
+
+
+)
+public class SmokeTestRunner {
+}
